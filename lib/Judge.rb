fbec60da80995ce58857d89e809c42b36fa4e418
Jeff <jmartinbaldwin@gmail.com>
2026-02-05 15:47:45 +0900

    fix judge evaluate logic

--- lib/Judge.rb
+++ lib/Judge.rb
@@ -4,6 +4,7 @@ class Judge
     white = 0
     temp_guess = []
     temp_secret = []
+
     secret.each_index do |i|
       
      if guess[i] == secret[i]
@@ -12,15 +13,18 @@ class Judge
       temp_guess << guess[i]
       temp_secret << secret[i]
      end  
+    end
 
-     temp_guess.each do |element|
-       if temp_secret.include?(element)
-          white += 1
-          index = temp_secret.find_index(element)
-          temp_secret.delete_at(index)
-       end 
-     end
+    temp_guess.each do |element|
+      if temp_secret.include?(element)
+        white += 1
+        index = temp_secret.find_index(element)
+        temp_secret.delete_at(index)
+      end 
     end 
+
+    {black: black, white: white}
+    
   end
 end
 
