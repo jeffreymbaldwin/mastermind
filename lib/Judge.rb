368855cfe6a7ceaa58ee48eb460963e99450a109
Jeff <jmartinbaldwin@gmail.com>
2026-02-05 14:59:08 +0900

    Implement black and white peg logic in Judge class

--- lib/Judge.rb
+++ lib/Judge.rb
@@ -1,5 +1,30 @@
 class Judge
   def evaluate(secret, guess)
-    
+    black = 0
+    white = 0
+    temp_guess = []
+    temp_secret = []
+    secret.each_index do |i|
+      
+     if guess[i] == secret[i]
+      black += 1
+     else 
+      temp_guess << guess[i]
+      temp_secret << secret[i]
+     end  
+
+     temp_guess.each do |element|
+       if temp_secret.include?(element)
+          white += 1
+          index = temp_secret.find_index(element)
+          temp_secret.delete_at(index)
+       end 
+     end
+
+
+    end 
   end
 end
+
+
+
