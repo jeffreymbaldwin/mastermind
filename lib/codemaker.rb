27ba51625a338660beb40c897785d33c03be00a7
Jeff <jmartinbaldwin@gmail.com>
2026-02-09 09:32:09 +0900

    add codemaker function

---
+++ lib/codemaker.rb
@@ -0,0 +1,21 @@
+
+
+
+class CodeMaker
+  attr_reader :secret
+  
+  def initialize
+    make_code
+  end
+
+
+  def make_code
+    options = ["A", "B", "C", "D", "E", "F"]
+
+    @secret = Array.new(4) {options.sample}
+  end
+end
+
+    #while @secret.length < 4
+      #choice = options.sample
+      #@secret << choice
--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,27 @@
-require_relative 'Judge'
+require_relative 'judge'
+require_relative 'codemaker'
 
 
 class Game
   def initialize
+    @codemaker = CodeMaker.new
+    @breaker
+    @humanbreaker
     @judge = Judge.new
+    @turn = 1
+    @history = []
   end
+
+  def play
+    @secret = @codemaker.secret
+    #loop until the game is done method
+      #ask breaker for guess
+      #ask judge to evaulate
+      #store result
+      #render
+      #increment turn
+  end
+
+
 end
 
